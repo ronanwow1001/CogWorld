b86f34a2b4999e4e306499fc5ed3af618ef40d2e
ronanwow1001 <ronancaleb123@gmail.com>
2017-08-24 11:18:01 -0700

    fixed

--- start_uberdog_server.bat
+++ start_uberdog_server.bat
@@ -1,55 +1,32 @@
 @echo off
 
-title Cogworld start_uberdog
-_server
+title Cogworld start_uberdog_server
 
-echo Choose your connection method!
-echo.
-echo #1 - Offline Mode
-echo.
-
-:selection
-
-set INPUT=-1
-set /P INPUT=Selection: 
-
-if %INPUT%==1 (
-    set TT_GAMESERVER=127.0.0.1
-) else (
-    goto selection
-)
-
-echo.
-
-if %INPUT%==1 (
-    set /P ttUsername="Username: "
-) else (
-    set /P TT_PLAYCOOKIE=Username: 
-)
-
-echo.
+rem Define some constants for our UberDOG server:
+set MAX_CHANNELS=999999
+set STATESERVER=4002
+set ASTRON_IP=127.0.0.1:7100
+set EVENTLOGGER_IP=127.0.0.1:7198
+set BASE_CHANNEL=1000000
 
 echo ===============================
-echo Starting Toontown 2.0
+echo Starting Toontown 2.0 UberDOG server...
 echo ppython: "C:\Panda3D-1.10.0\python\ppython.exe"
-
-if %INPUT%==1 (
-    echo Username: %ttUsername%
-) else (
-    echo Username: %TT_PLAYCOOKIE%
-)
-
-echo Gameserver: %TT_GAMESERVER%
+echo Base channel: %BASE_CHANNEL%
+echo Max channels: %MAX_CHANNELS%
+echo State Server: %STATESERVER%
+echo Astron IP: %ASTRON_IP%
+echo Event Logger IP: %EVENTLOGGER_IP%
 echo ===============================
 
 cd ../../
 
 :main
-if %INPUT%==1 (
-    "C:\Panda3D-1.10.0\python\ppython.exe" -m toontown.toonbase.ToontownStart
-) else (
-    "C:\Panda3D-1.10.0\python\ppython.exe" -m toontown.toonbase.ToontownStart
-)
-pause
-
+"C:\Panda3D-1.10.0\python\ppython.exe" ^
+	-m toontown.uberdog.ServiceStart ^
+	--base-channel %BASE_CHANNEL% ^
+	--max-channels %MAX_CHANNELS% ^
+	--stateserver %STATESERVER% ^
+	--astron-ip %ASTRON_IP% ^
+	--eventlogger-ip %EVENTLOGGER_IP%
 goto main
